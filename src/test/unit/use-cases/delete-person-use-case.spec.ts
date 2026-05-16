c6134ce1394e443f5236643ed196417d362c5fba
osmarmcn <osmarn84@gmail.com>
2026-05-16 17:51:30 -0300

    feat: criando os testes do update e delete

---
+++ src/test/unit/use-cases/delete-person-use-case.spec.ts
@@ -0,0 +1,52 @@
+
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { Person } from '@/domain/entities/person'
+import { DeletePersonUseCase } from '@/domain/use-cases/deletePersonUseCase'
+import { InMemoryPersonRepository } from '@/test/repositories/in-memory-person-repository'
+
+
+
+let repository: InMemoryPersonRepository
+let sut: DeletePersonUseCase
+
+describe('Delete Person Use Case', () => {
+  beforeEach(() => {
+    repository = new InMemoryPersonRepository()
+
+    sut = new DeletePersonUseCase(repository)
+  })
+
+  it('should be able delete a person', async () => {
+    const createdPerson =
+      await repository.create(
+        new Person(
+          crypto.randomUUID(),
+          'João',
+          'joao@email.com',
+          '12345678900',
+          '85999999999',
+          'Rua A',
+          'male',
+          'developer',
+          'college',
+        ),
+      )
+
+    await sut.execute({
+      id: createdPerson.id,
+    })
+
+    const people = await repository.findAll()
+
+    expect(people).toHaveLength(0)
+  })
+
+  it('should not delete nonexistent person', async () => {
+    await expect(() =>
+      sut.execute({
+        id: 'nonexistent-id',
+      }),
+    ).rejects.toThrow('Person not found')
+  })
+})
---
+++ src/test/unit/use-cases/update-person-use-case.spec.ts
@@ -0,0 +1,57 @@
+
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { Person } from '@/domain/entities/person'
+import { InMemoryPersonRepository } from '@/test/repositories/in-memory-person-repository'
+import { UpdatePersonUseCase } from '@/domain/use-cases/updatePersonUseCase'
+
+
+
+let repository: InMemoryPersonRepository
+let sut: UpdatePersonUseCase
+
+describe('Update Person Use Case', () => {
+  beforeEach(() => {
+    repository = new InMemoryPersonRepository()
+
+    sut = new UpdatePersonUseCase(repository)
+  })
+
+  it('should be able update a person', async () => {
+    const createdPerson =
+      await repository.create(
+        new Person(
+          crypto.randomUUID(),
+          'João',
+          'joao@email.com',
+          '12345678900',
+          '85999999999',
+          'Rua A',
+          'male',
+          'developer',
+          'college',
+        ),
+      )
+
+    const updatedPerson =
+      await sut.execute({
+        id: createdPerson.id,
+
+        name: 'João Atualizado',
+      })
+
+    expect(updatedPerson.name).toEqual(
+      'João Atualizado',
+    )
+  })
+
+  it('should not update nonexistent person', async () => {
+    await expect(() =>
+      sut.execute({
+        id: 'nonexistent-id',
+
+        name: 'Teste',
+      }),
+    ).rejects.toThrow('Person not found')
+  })
+})
